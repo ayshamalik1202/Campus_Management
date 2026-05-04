9604ed4dd8de5e20ef6516201a737403798c7056
Aysha Akther Tanni <ayshamalik1202@gmail.com>
2026-05-04 15:24:34 +0600

    Update NoticeObserver.java

--- src/interfaces/NoticeObserver.java
+++ src/interfaces/NoticeObserver.java
@@ -1,8 +1,6 @@
 package interfaces;
 
 import models.Notice;
-
-// FIX: This interface was referenced in NoticeManager but never created
 public interface NoticeObserver {
     void onNoticeReceived(Notice notice);
 }
