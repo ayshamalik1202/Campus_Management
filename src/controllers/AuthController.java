932e9b2dfc4c65187c525885618090f3133a4ab9
Aysha Akther Tanni <ayshamalik1202@gmail.com>
2026-05-04 15:23:02 +0600

    Update AuthController.java

--- src/controllers/AuthController.java
+++ src/controllers/AuthController.java
@@ -4,8 +4,6 @@ import models.User;
 import utils.DataStore;
 
 public class AuthController {
-    // NOTE: Passwords are compared as plaintext here.
-    // For production, use BCrypt or similar hashing before storing/comparing.
     public User login(String id, String password) {
         User user = DataStore.getInstance().getUser(id);
         if (user != null && user.getPassword().equals(password)) {
